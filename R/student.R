faf232793eea447a55bdf348d27619c33b9e7938
stla <laurent_step@yahoo.fr>
2017-12-10 15:45:04 +0100

    keep example

--- R/student.R
+++ R/student.R
@@ -33,6 +33,11 @@
 #' apply(Tsims, 1:2, mean) # approximates M
 #' vecTsims <- t(apply(Tsims, 3, function(X) c(t(X))))
 #' round(cov(vecTsims), 1) # approximates 1/(nu-2) * kronecker(U,V)
+#' ## the `keep` class is nice when m=1 or p=1:
+#' Tsims <- rmatrixt(2, nu, M=1:3, U=diag(3), V=1)
+#' Tsims[,,1] # dimensions 3 1
+#' # without `keep`, dimensions are lost:
+#' rmatrixt(2, nu, M=1:3, U=diag(3), V=1, keep=FALSE)[,,1]
 rmatrixt <- function(n, nu, M, U, V, checkSymmetry=TRUE, keep=TRUE){
   if(!isPositiveInteger(n)){
     stop("`n` must be a positive integer")
